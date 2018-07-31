106f8402ef07acadd7975496f98efcfbaf583018
Zaki Sediqyar <zaki.sediqyar@gmail.com>
2018-07-31 06:04:51 +0000

    temperature done

--- js/main.js
+++ js/main.js
@@ -12,3 +12,36 @@ function showEmResult () {
   var emResult = document.getElementById("emResult");
   emResult.innerHTML = pixeled;
 }
+
+
+//********** Temperature ****************
+function celToFahLogic () {
+  var celBaseValue = 32;
+  var celValue = document.getElementById("celsius").value;
+  
+  return celValue * 1.8 + 32;
+}
+
+function celToFahUi () {
+  var celToFahed = celToFahLogic();
+  var celToFahResult = document.getElementById("celToFahResult");
+  
+  celToFahResult.innerHTML = celToFahed;
+}
+
+
+//************ Anagram Finder ****************
+function anagramLogic (a,b) {
+  var firstWord = document.getElementById("firstWord").value;
+  var secondWord = document.getElementById("secondWord").value;
+  var anagramResult = document.getElementById("anagramResult");
+  
+  firstWord = a.split("").sort().join("");
+  secondWord = b.split("").sort().join("");
+  
+  if (firstWord == secondWord) {
+    anagramResult.innerHTML = "Yay!! " + firstWord + " & " + secondWord + " are Anagrams.";
+  } else {
+    anagramResult.innerHTML = "Oops!! They seem to not be Anagrams :(";
+  }
+}
